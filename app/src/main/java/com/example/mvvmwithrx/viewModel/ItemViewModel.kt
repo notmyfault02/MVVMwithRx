ec2dc24114a25fc597c1d56e475ce4e83f455fcd
김영래 <rayray.yoyo@gmail.com>
2019-04-19 15:08:27 +0900

    Add MVVM

--- app/src/main/java/com/example/mvvmwithrx/viewModel/ItemViewModel.kt
+++ app/src/main/java/com/example/mvvmwithrx/viewModel/ItemViewModel.kt
@@ -1,14 +1,17 @@
 package com.example.mvvmwithrx.viewModel
 
-import android.content.Context
+import android.arch.lifecycle.MutableLiveData
 import android.databinding.BaseObservable
 import android.databinding.BindingAdapter
 import android.view.View
 import android.widget.ImageView
 import com.bumptech.glide.Glide
-import com.example.mvvmwithrx.model.HistoricalSite
 
-class ItemViewModel(val history: HistoricalSite ,val context: Context, val constract: Constract): BaseObservable() {
+class ItemViewModel(val constract: Constract): BaseObservable() {
+
+    val historicalSiteName = MutableLiveData<String>()
+    val historicalSiteLocation = MutableLiveData<String>()
+
     @BindingAdapter("historicalSiteImagePath")
     fun setImageUrl(imageView: ImageView, url: String) {
         Glide.with(imageView.context).load(url).into(imageView)
